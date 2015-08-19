e8837621d82a689b5496703ab1cca8ac3caa6283
Adriano Valente <dri9595@gmail.com>
2015-08-19 13:39:37 -0300

    adding get name for model method

--- modelResults.py
+++ modelResults.py
@@ -6,6 +6,9 @@ def isSingleType(instanceObject):
             return True
     return False
 
+def getModelNameForKey(key):
+    return str(key).title() + "Model"
+
 class ModelResults:
     def __init__(self):
         self.interfaces = "//\n//  {{Type your classe name here}}.h\n//  {{Type your project name here}}\n//\n//  Created by json-model-generator.\n//  Copyright (c) 2015 Taqtile. All rights reserved.\n//\n\n#import \"JSONModel.h\""
@@ -13,18 +16,23 @@ class ModelResults:
         self.implementations = ""
 
     def addInterfaceWithDictionary(self, dictionary, name):
-        self.interfaces += "\n@interface " + name + " : JSONModel"
-        self.protocols += "@protocol " + name + " <NSObject>"
+        self.interfaces += "\n\n@interface " + name + " : JSONModel\n"
+        self.protocols += "\n@protocol " + name + " <NSObject>\n"
 
         for key in dictionary:
 
             #TODO: Add logic to recursively call addInterfaceWithDictionary
+
+            if (isinstance(dictionary[key], dict)):
+                self.interfaces += "@property (nonatomic) id <" + getModelNameForKey(key) + "> " + str(key) + ";\n"
+
+
             if (isSingleType(dictionary[key])):
                 self.interfaces += SINGLE_TYPES[type(dictionary[key])] + str(key) + ";\n"
 
 
-        self.interfaces += "@end"
-        self.protocols += "@end"
+        self.interfaces += "@end\n"
+        self.protocols += "@end\n"
 
     def getInterfaces(self):
         return self.interfaces
